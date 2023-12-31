e463619c1e0b675173678592a8b9dc6983477585
tibaas <igorox2@hotmail.com>
2023-12-31 15:14:33 -0300

    Updating

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import { Dashboard } from "./pages/app/dashboard";
 import { SignIn } from "./pages/auth/sign-in";
 import { AppLayout } from "./pages/_layouts/app";
+import { AuthLayout } from "./pages/_layouts/auth";
 
 export const router = createBrowserRouter([
 
@@ -14,7 +15,7 @@ export const router = createBrowserRouter([
     },
     {
         path: '/',
-        element: <AppLayout />,
+        element: <AuthLayout />,
         children: [
             {path: '/sign-in', element: <SignIn />}
         ]
