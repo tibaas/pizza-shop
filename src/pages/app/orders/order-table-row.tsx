b8e5ec9bec33284edd50af3e23f245e1f0df8c2f
tibaas <igorox2@hotmail.com>
2023-12-31 20:19:41 -0300

    Updating

---
+++ src/pages/app/orders/order-table-row.tsx
@@ -0,0 +1,46 @@
+import { Button } from "@/components/ui/button";
+import { TableCell, TableRow } from "@/components/ui/table";
+import { ArrowRight, Search, X } from "lucide-react";
+
+export function OrderTableRow() {
+    return (
+        <TableRow>
+            <TableCell>
+                <Button variant="outline" size="xs">
+                    <Search className="h-3 w-3" />
+                    <span className="sr-only">Detalhes do pedido</span>
+                </Button>
+            </TableCell>
+            <TableCell className="font-mono text-sm font-medium">
+                54a5sdas12asdazdasfas123
+            </TableCell>
+            <TableCell className="text-muted-foreground"> 
+                Há 15 minutos
+            </TableCell>
+            <TableCell>
+                <div className="flex items-center gap-2">
+                    <span className="h-2 w-2 rounded-full bg-slate-400" />
+                    <span className="font-medium text-muted-foreground">Pendente</span>
+                </div>
+            </TableCell>
+            <TableCell className="font-medium"> 
+                Igor Tibério 
+            </TableCell>
+            <TableCell className="font-medium">
+                R$ 149,90
+            </TableCell>
+            <TableCell>
+            <Button variant="outline" size="xs">
+                    <ArrowRight className="h-3 w-3 mr-2" /> 
+                    Aprovar
+                </Button>
+            </TableCell>
+            <TableCell>
+                <Button variant="ghost" size="xs">
+                    <X className="h-3 w-3 mr-2" /> 
+                    Cancelar 
+                </Button>
+            </TableCell>
+        </TableRow>
+    )
+}
--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -1,82 +1,43 @@
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
-import { ArrowRight, Search, X } from "lucide-react"
+
+import { Table, TableBody, TableHead, TableHeader, TableRow } from "@/components/ui/table"
+
 import { Helmet } from "react-helmet-async"
+import { OrderTableRow } from "./order-table-row"
+import { OrderTableFilters } from "./order-table-filters"
+import { Pagination } from "@/components/pagination"
 
 export function Orders() {
     return (
         <>
             <Helmet title="Pedidos" />
             <div className="flex flex-col gap-4">   
-                <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
-            </div>
-            <div className="space-y-2.5">
-                <form className="flex items-center gap-2">
-                    <span className="text-sm font-semibold">Filtros:</span>
-                    <Input placeholder="Nome do cliente" className="h-8 w-[320px]" />
-                </form>
-
-                <div className="border rounded-md">
-                    <Table>
-                        <TableHeader>
-                            <TableRow>
-                                <TableHead className="w-[64px]"></TableHead>
-                                <TableHead className="w-[140px]">Idenficador</TableHead>
-                                <TableHead className="w-[180px]">Realizado há</TableHead>
-                                <TableHead className="w-[140px]">Status</TableHead>
-                                <TableHead>Cliente</TableHead>
-                                <TableHead className="w-[140px]">Total do pedido</TableHead>
-                                <TableHead className="w-[164px]"></TableHead>
-                                <TableHead className="w-[132px]"></TableHead>
-                            </TableRow>
-                        </TableHeader>
-                        <TableBody>
-                           {Array.from({ length: 10}).map((_, i) => {
-                            return (
-                                <TableRow key={i}>
-                                    <TableCell>
-                                        <Button variant="outline" size="xs">
-                                            <Search className="h-3 w-3" />
-                                            <span className="sr-only">Detalhes do pedido</span>
-                                        </Button>
-                                    </TableCell>
-                                    <TableCell className="font-mono text-sm font-medium">
-                                        54a5sdas12asdazdasfas123
-                                    </TableCell>
-                                    <TableCell className="text-muted-foreground"> 
-                                        Há 15 minutos
-                                    </TableCell>
-                                    <TableCell>
-                                        <div className="flex items-center gap-2">
-                                            <span className="h-2 w-2 rounded-full bg-slate-400" />
-                                            <span className="font-medium text-muted-foreground">Pendente</span>
-                                        </div>
-                                    </TableCell>
-                                    <TableCell className="font-medium"> 
-                                        Igor Tibério 
-                                    </TableCell>
-                                    <TableCell className="font-medium">
-                                        R$ 149,90
-                                    </TableCell>
-                                    <TableCell>
-                                    <Button variant="outline" size="xs">
-                                            <ArrowRight className="h-3 w-3 mr-2" /> 
-                                            Aprovar
-                                        </Button>
-                                    </TableCell>
-                                    <TableCell>
-                                        <Button variant="ghost" size="xs">
-                                            <X className="h-3 w-3 mr-2" /> 
-                                            Cancelar 
-                                        </Button>
-                                    </TableCell>
+                <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>          
+                <div className="space-y-2.5">
+                    <OrderTableFilters />
+                    <div className="border rounded-md">
+                        <Table>
+                            <TableHeader>
+                                <TableRow>
+                                    <TableHead className="w-[64px]"></TableHead>
+                                    <TableHead className="w-[140px]">Idenficador</TableHead>
+                                    <TableHead className="w-[180px]">Realizado há</TableHead>
+                                    <TableHead className="w-[140px]">Status</TableHead>
+                                    <TableHead>Cliente</TableHead>
+                                    <TableHead className="w-[140px]">Total do pedido</TableHead>
+                                    <TableHead className="w-[164px]"></TableHead>
+                                    <TableHead className="w-[132px]"></TableHead>
                                 </TableRow>
-                            )})}
-                        </TableBody>
-                    </Table>
-                </div>
+                            </TableHeader>
+                            <TableBody>
+                            {Array.from({ length: 10}).map((_, i) => {
+                                return <OrderTableRow key={i} /> 
+                                })}
+                            </TableBody>
+                        </Table>
+                    </div>
 
+                    <Pagination pageIndex={0} totalCount={105} perPage={10} />
+                </div>               
             </div>
         </>
     )
